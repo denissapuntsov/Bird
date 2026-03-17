0a959f3b27adb99c6f8d2e65537fd2e1bfec968f
Denis Sapuntsov <125272025+denissapuntsov@users.noreply.github.com>
2026-03-17 15:56:43 +0000

    Moved analyser to editor menu

--- Assets/Analysis/AnalyserMenu.cs
+++ Assets/Analysis/AnalyserMenu.cs
@@ -1,20 +1,83 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 using NAudio.Wave;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-public class AudioFileAnalyser : MonoBehaviour
+public class AnalyserMenu : EditorWindow
 {
-    [SerializeField] private AudioClip audioClip;
+    public AudioClip audioClip;
     private string _outputFile, _inputFile;
+    public List<Cue> cues = new List<Cue>();
+    public List<Cue> cuesToAdd;
 
-    [SerializeField] private List<Cue> cues;
-    [SerializeField] private List<Cue> cuesToAdd;
+    [MenuItem("Window/.wav File Analyser")]
+    public static void ShowWindow()
+    {
+        GetWindow<AnalyserMenu>("Analyser");
+    }
 
-    public void UpdateCues() => UpdateCues(false);
-    
+    private void CreateGUI()
+    {
+        cues = new List<Cue>() {new Cue("test", 122, 44100)};
+        var list = new ListView();
+        rootVisualElement.Add(list);
+        list.makeItem = () => new Label();
+        list.bindItem = (item, index) => { (item as Label).text = cues[index].label; };
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Space(10);
+        GUILayout.Label("Analyser", EditorStyles.boldLabel);
+        
+        GUILayout.Space(10);
+        EditorGUILayout.BeginHorizontal();
+        audioClip = EditorGUILayout.ObjectField(audioClip, typeof(AudioClip), true) as AudioClip;
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(10);
+        AddProperty("cues");
+
+        GUILayout.Space(10);
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Update"))
+        {
+            UpdateCues();
+        }
+
+        if (GUILayout.Button("Clear"))
+        {
+            RemoveAllCues();
+        }
+
+        if (GUILayout.Button("Add From List"))
+        {
+            AddCuesFromList();
+        }
+        EditorGUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        SerializedProperty cueProperty;
+        SerializedProperty cuesToAddProperty;
+
+        AddProperty("cuesToAdd");
+    }
+
+    private void AddProperty(string propertyToFind)
+    {
+        ScriptableObject target = this;
+        SerializedObject so = new SerializedObject(target);
+        SerializedProperty serializedProperty = so.FindProperty(propertyToFind);
+        
+        EditorGUILayout.PropertyField(serializedProperty, true);
+        so.ApplyModifiedProperties();
+    }
+
+    private void UpdateCues() => UpdateCues(false);
+
     private void UpdateCues(bool addFromList)
     {
         _inputFile = AssetDatabase.GetAssetPath(audioClip);
@@ -71,21 +134,22 @@ public class AudioFileAnalyser : MonoBehaviour
                         {
                             cue.positionInSamples = (int)(reader.WaveFormat.SampleRate * audioClip.length);
                         }
+
                         writer.AddCue(cue.positionInSamples, cue.label);
                     }
 
                     cuesToAdd.Clear();
                 }
-                
+
                 writer.Flush();
                 writer.Close();
             }
         }
-        
+
         File.Delete(_outputFile);
-        
+
         AssetDatabase.Refresh();
-        
+
         UpdateCueList();
     }
 
@@ -104,7 +168,7 @@ public class AudioFileAnalyser : MonoBehaviour
                 }
             }
         }
-        
+
         UpdateScriptableObject();
     }
 
@@ -117,7 +181,7 @@ public class AudioFileAnalyser : MonoBehaviour
         {
             newAudioData.cuePoints.Add(cue);
         }
-        
+
         AssetDatabase.CreateAsset(newAudioData, $"Assets/Analysis/CuePoints/{audioClip.name}_CuePoints.asset");
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
@@ -140,7 +204,7 @@ public class AudioFileAnalyser : MonoBehaviour
                 writer.Close();
             }
         }
-        
+
         using (CueWaveFileReader reader = new CueWaveFileReader(_outputFile))
         {
             using (CueWaveFileWriter writer = new CueWaveFileWriter(_inputFile, reader.WaveFormat))
@@ -151,23 +215,24 @@ public class AudioFileAnalyser : MonoBehaviour
                 {
                     writer.Write(buffer, 0, read);
                 }
-                
+
                 writer.Flush();
                 writer.Close();
             }
         }
-        
+
         File.Delete(_outputFile);
 
         UpdateCues();
-        
+
         AssetDatabase.Refresh();
-        
+
         UpdateScriptableObject();
+
     }
 }
 
-/*[Serializable]
+[Serializable]
 public class Cue
 {
     public string label;
@@ -175,9 +240,9 @@ public class Cue
     public float positionInSeconds;
 
     public Cue(string label, int positionInSamples, int sampleRate)
-    {
-        this.label = label;
+    { 
+        this.label = label; 
         this.positionInSamples = positionInSamples;
         positionInSeconds = (float)positionInSamples / sampleRate;
     }
-}*/
+}
