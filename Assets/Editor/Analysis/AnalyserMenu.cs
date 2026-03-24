4e6f5f811bd19f48a3912a92fcb9a43e87d82c3f
Denis Sapuntsov <125272025+denissapuntsov@users.noreply.github.com>
2026-03-24 02:50:10 +0000

    Added waveform rendering and update on changing audio clip value

--- Assets/Editor/Analysis/AnalyserMenu.cs
+++ Assets/Editor/Analysis/AnalyserMenu.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using NAudio.Wave;
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,6 +17,7 @@ public class AnalyserMenu : EditorWindow
     public List<Cue> cuesToAdd;
     private int toolbarInt;
     private string[] toolbarStrings = new string[] { "Update/Delete", "Add From List" };
+    private Texture2D _waveform;
 
     [MenuItem("Window/.wav File Analyser")]
     public static void ShowWindow()
@@ -35,8 +38,45 @@ public class AnalyserMenu : EditorWindow
     {   
         GUILayout.Space(10);
         EditorGUILayout.BeginHorizontal();
+        
+        EditorGUI.BeginChangeCheck();
         audioClip = EditorGUILayout.ObjectField(audioClip, typeof(AudioClip), true) as AudioClip;
+        if (EditorGUI.EndChangeCheck())
+        {
+            // check if scriptable object for file exists, and if so, show the values as list; 
+            // otherwise clear list
+            
+            RhythmData rhythmDataAsset = null;
+            if (audioClip != null)
+            {
+                Debug.Log("new non-empty audio clip");
+                rhythmDataAsset = AssetDatabase.LoadAssetAtPath<RhythmData>($"Assets/CuePoints/{audioClip.name}_CuePoints.asset");
+            }
+
+            if (!rhythmDataAsset)
+            {
+                cues = null;
+            }
+            else
+            {
+                cues = rhythmDataAsset.cuePoints;
+            }
+
+            _waveform = DrawWaveformTexture(audioClip, (int)EditorGUIUtility.currentViewWidth - 10, 40);
+        }
+
+        
         EditorGUILayout.EndHorizontal();
+        
+        if (audioClip)
+        {
+            GUILayout.Space(10);
+            float imageWidth = EditorGUIUtility.currentViewWidth - 10;
+            float imageHeight = 40;
+            Rect rect = GUILayoutUtility.GetRect(imageHeight, imageHeight);
+            GUI.DrawTexture(rect, _waveform, ScaleMode.ScaleToFit, alphaBlend:true);
+        }
+        
         GUILayout.Space(10);
         toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings, GUILayout.Height(20));
         GUILayout.Space(10);
@@ -263,5 +303,46 @@ public class AnalyserMenu : EditorWindow
         Color lineColor = new Color(0.10196f, 0.10196f, 0.10196f, 1);
         EditorGUI.DrawRect(rect, lineColor);
         GUILayout.Space(4);
+    }
+
+    private Texture2D DrawWaveformTexture(AudioClip audioClip, int width, int height)
+    {
+        if (!audioClip) return null;
+        
+        Texture2D newTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        float[] samples = new float[audioClip.samples];
+        float[] waveform = new float[width];
+        audioClip.GetData(samples, 0);
+        int packSize = (audioClip.samples / width) + 1;
+        int s = 0;
+        for (int i = 0; i < samples.Length; i += packSize)
+        {
+            waveform[s] = Mathf.Abs(samples[i]);
+            s++;
+        }
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                newTexture.SetPixel(x, y, EditorGUIUtility.isProSkin
+                    ? (Color) new Color32 (56, 56, 56, 255)
+                    : (Color) new Color32 (194, 194, 194, 255));
+            }
+        }
+
+        for (int x = 0; x < waveform.Length; x++)
+        {
+            for (int y = 0; y <= waveform[x] * ((float)height * .75f); y++)
+            {
+                newTexture.SetPixel(x, (height / 2) + y, new Color(1.0f, 0.64f, 0.0f, 1f));
+                newTexture.SetPixel(x, (height / 2) - y, new Color(1.0f, 0.64f, 0.0f, 1f));
+            }
+        }
+
+        newTexture.Apply();
+
+        return newTexture;
     }
 }
